c4bbfa26649c888357dd110e29a135ad46f396b1
sendyakbar <msendyakbars@gmail.com>
2023-02-07 17:12:07 +0700

    create: promo card component

---
+++ src/components/PromoCard.component.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {colors} from '../theme/colors';
+
+const {width: deviceWidth} = Dimensions.get('window');
+
+export default function PromoCard(props) {
+  const {imageUri, title, description} = props;
+
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
+      <Image source={{uri: imageUri}} style={styles.image} />
+      <View style={styles.textCol}>
+        <Text style={styles.titleText}>{title}</Text>
+        <Text style={styles.descText}>{description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+    backgroundColor: colors.WHITE,
+    shadowColor: colors.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    borderRadius: 18,
+    marginTop: 16,
+  },
+  image: {
+    width: '100%',
+    height: (deviceWidth - 32) * 0.5,
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+  },
+  textCol: {
+    padding: 16,
+    backgroundColor: colors.WHITE,
+    borderBottomRightRadius: 18,
+    borderBottomLeftRadius: 18,
+  },
+  titleText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: colors.BLACK,
+  },
+  descText: {
+    fontSize: 11,
+    fontWeight: '300',
+    color: colors.BLACK,
+    marginTop: 4,
+  },
+});
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -5,6 +5,7 @@ import BannerCarousel from '../components/BannerCarousel.component';
 import GoPayBox from '../components/GoPayBox.component';
 import MainMenu from '../components/MainMenu.component';
 import MenuSlider from '../components/MenuSlider.component';
+import PromoCard from '../components/PromoCard.component';
 import TreasureBox from '../components/TreasureBox.component';
 import {colors} from '../theme/colors';
 
@@ -42,6 +43,26 @@ export default function HomeScreen() {
           },
         ]}
       />
+      <PromoCard
+        imageUri="https://lelogama.go-jek.com/cms_editor/2021/04/14/top-banner-1-ID.jpg"
+        title="Pertolongan pertama buat ungkapin cinta"
+        description="Beli coklat di GoMart, dianternya cepat. Nikmatin diskon ongkirnya"
+      />
+      <PromoCard
+        imageUri="https://lelogama.go-jek.com/post_featured_image/Consumer_Trends_Blog_Banner_1.jpg"
+        title="Kebutuhan apa aja, pasti ada!"
+        description="Kamu butuh sesuatu, GoMart antar gak pake ntar-ntar. Klik!"
+      />
+      <PromoCard
+        imageUri="https://1.bp.blogspot.com/--T4z8RjiFAw/YUC_YQTGdqI/AAAAAAAACzE/mSbSMMQ92iM4Z2HUHjpf3IVlPDii8eB8ACLcBGAsYHQ/w0/gosend.webp"
+        title="Solusi perjalanan hemat"
+        description="Yuk pake fitur GoRide Hemat buat trip jarak dekat. Klik di sini!"
+      />
+      <PromoCard
+        imageUri="https://lelogama.go-jek.com/post_featured_image/supe_app_header_1.jpg"
+        title="Cashback 75% GoPay dari GoSend!"
+        description="Masukin kode GOSENDHEMAT dan dapatkan Cashback 75% GoPay Coins. Yuk, GoSend-in sekarang!"
+      />
     </ScrollView>
   );
 }
@@ -50,5 +71,6 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     backgroundColor: colors.WHITE,
+    paddingBottom: 16,
   },
 });
